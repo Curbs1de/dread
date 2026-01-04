9b741bec70eb81d8d1010975f0fe2c5247b4fdb2
Curbs1de <lakedrakecake@gmail.com>
2026-01-03 22:51:51 -0500

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,1 +1,20 @@
+from discord.ext import commands
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="freeze")
+    @commands.has_permissions(manage_channels=True)
+    async def freeze(self, ctx):
+        for role in ctx.guild.roles:
+            await ctx.channel.set_permissions(role, send_messages=False)
+
+    @commands.command(name="clone")
+    @commands.has_permissions(manage_channels=True)
+    async def clone(self, ctx):
+        await ctx.channel.clone()
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
 
